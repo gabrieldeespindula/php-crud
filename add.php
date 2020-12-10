d77323c1e3d793d28ac36605897597628e423b66
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-12-10 15:13:31 -0300

    Creating add.php

---
+++ add.php
@@ -0,0 +1,17 @@
+<?php
+// header
+include_once "includes/header.php";
+?>
+
+<div class="row">
+    <div class="col s12 m6 push-m3">
+        <h3 class="light">New Client</h3>
+
+    </div>
+</div>
+
+
+<?php
+//footer
+include_once "includes/footer.php";
+?>
