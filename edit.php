e276129b78f4395b28e6330ce715d8467216b07e
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-12-11 16:03:44 -0300

    Getting information by id.

--- edit.php
+++ edit.php
@@ -3,6 +3,14 @@
 include_once 'phpaction/dbconnect.php';
 // header
 include_once "includes/header.php";
+// select
+if(isset($_GET['id'])):
+    $id = mysqli_escape_string($connect, $_GET['id']);
+
+    $sql = "SELECT * FROM client WHERE id = '$id'";
+    $result = mysqli_query($connect, $sql);
+    $data = mysqli_fetch_array($result);
+endif;
 ?>
 
 <div class="row">
@@ -10,23 +18,23 @@ include_once "includes/header.php";
         <h3 class="light">Edit Client</h3>
         <form action="phpaction/update.php" method="POST">
             <div class="input-field col s12">
-                <input type="text" name="name" id="name">
+                <input type="text" name="name" id="name" value="<?php echo $data['name'] ?>">
                 <label for="name">Name</label>
             </div>
             <div class="input-field col s12">
-                <input type="text" name="lastname" id="lastname">
+                <input type="text" name="lastname" id="lastname" value="<?php echo $data['lastname'] ?>">
                 <label for="lastname">Last name</label>
             </div>
             <div class="input-field col s12">
-                <input type="text" name="email" id="email">
+                <input type="text" name="email" id="email" value="<?php echo $data['email'] ?>">
                 <label for="email">Email</label>
             </div>
             <div class="input-field col s12">
-                <input type="text" name="age" id="age">
+                <input type="text" name="age" id="age" value="<?php echo $data['age'] ?>">
                 <label for="age">Age</label>
             </div>
 
-            <button type="submit" name="btn-register" class="btn"> Register </button>
+            <button type="submit" name="btn-register" class="btn"> Update </button>
             <a href="index.php" class="btn green"> Client List </a>
         </form>
     </div>
